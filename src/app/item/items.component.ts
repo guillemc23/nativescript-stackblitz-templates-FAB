268251297c349dee3a013dc8fb6e928959a97191
guillemc23 <guillemoreno.95@gmail.com>
2023-02-18 13:27:23 +0100

    5 files modified, 2 added, 5 deleted

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -1,24 +1,87 @@
-import { Component, OnInit } from '@angular/core';
-import { Fab } from '@nstudio/nativescript-floatingactionbutton';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { registerElement } from '@nativescript/angular';
 registerElement(
-    'Fab',
-    () => require('@nstudio/nativescript-floatingactionbutton').Fab
+  'Fab',
+  () => require('@nstudio/nativescript-floatingactionbutton').Fab
 );
 
-import { Item } from './item';
-import { ItemService } from './item.service';
+import { View } from '@nativescript/core';
 
 @Component({
   selector: 'ns-items',
   templateUrl: './items.component.html',
+  styleUrls: ['./items.component.scss'],
 })
 export class ItemsComponent implements OnInit {
-  items: Array<Item>;
+  constructor() {}
 
-  constructor(private itemService: ItemService) {}
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-    this.items = this.itemService.getItems();
+  public _isFabOpen: boolean;
+  @ViewChild('btna') btna: ElementRef;
+  @ViewChild('btnb') btnb: ElementRef;
+  @ViewChild('fab') fab: ElementRef;
+
+  displayOptions() {
+    console.log('FAB tapped');
+    if (this._isFabOpen) {
+      // Rotate main fab
+      const fab = <View>this.fab.nativeElement;
+      fab.animate({ rotate: 0, duration: 280, delay: 0 });
+
+      // Show option 1
+      const btna = <View>this.btna.nativeElement;
+      btna.animate({
+        translate: { x: 0, y: 0 },
+        opacity: 0,
+        duration: 280,
+        delay: 0,
+      });
+
+      // Show option 2
+      const btnb = <View>this.btnb.nativeElement;
+      btnb.animate({
+        translate: { x: 0, y: 0 },
+        opacity: 0,
+        duration: 280,
+        delay: 0,
+      });
+
+      this._isFabOpen = false;
+    } else {
+      // Rotate main fab
+      const view = <View>this.fab.nativeElement;
+      view.animate({ rotate: 45, duration: 280, delay: 0 });
+
+      // Show option 1
+      const btna = <View>this.btna.nativeElement;
+      btna.animate({
+        translate: { x: 0, y: -80 },
+        opacity: 1,
+        duration: 280,
+        delay: 0,
+      });
+
+      // Show option 2
+      const btnb = <View>this.btnb.nativeElement;
+      btnb
+        .animate({
+          translate: { x: 0, y: -80 },
+          opacity: 1,
+          duration: 280,
+          delay: 0,
+        })
+        .then(() =>
+          btnb.animate({
+            translate: { x: 0, y: -145 },
+            opacity: 1,
+            duration: 280,
+            delay: 0,
+          })
+        );
+      // btnb.animate({ translate: { x: 0, y: -65 }, opacity: 1, duration: 280, delay: 140 });
+
+      this._isFabOpen = true;
+    }
   }
 }
