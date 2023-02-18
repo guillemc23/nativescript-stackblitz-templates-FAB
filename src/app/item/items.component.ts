a9e3222e12ec3d5dc651d56bc3c847b395d9b874
guillemc23 <guillemoreno.95@gmail.com>
2023-02-18 18:38:36 +0100

    3 files modified

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -5,7 +5,7 @@ registerElement(
   () => require('@nstudio/nativescript-floatingactionbutton').Fab
 );
 
-import { View } from '@nativescript/core';
+import { Animation, CoreTypes, View } from '@nativescript/core';
 
 @Component({
   selector: 'ns-items',
@@ -19,7 +19,11 @@ export class ItemsComponent implements OnInit {
 
   public _isFabOpen: boolean;
   @ViewChild('btna') btna: ElementRef;
+  @ViewChild('labela') labela: ElementRef;
   @ViewChild('btnb') btnb: ElementRef;
+  @ViewChild('labelb') labelb: ElementRef;
+  @ViewChild('btnc') btnc: ElementRef;
+  @ViewChild('labelc') labelc: ElementRef;
   @ViewChild('fab') fab: ElementRef;
 
   displayOptions() {
@@ -38,9 +42,41 @@ export class ItemsComponent implements OnInit {
         delay: 0,
       });
 
+      const labela = <View>this.labela.nativeElement;
+      labela.animate({
+        translate: { x: 0, y: 0 },
+        opacity: 0,
+        duration: 280,
+        delay: 0,
+      });
+
       // Show option 2
       const btnb = <View>this.btnb.nativeElement;
       btnb.animate({
+        translate: { x: 0, y: 0 },
+        opacity: 0,
+        duration: 280,
+        delay: 0,
+      });
+
+      const labelb = <View>this.labelb.nativeElement;
+      labelb.animate({
+        translate: { x: 0, y: 0 },
+        opacity: 0,
+        duration: 280,
+        delay: 0,
+      });
+      // Show option 3
+      const btnc = <View>this.btnc.nativeElement;
+      btnc.animate({
+        translate: { x: 0, y: 0 },
+        opacity: 0,
+        duration: 280,
+        delay: 0,
+      });
+
+      const labelc = <View>this.labelc.nativeElement;
+      labelc.animate({
         translate: { x: 0, y: 0 },
         opacity: 0,
         duration: 280,
@@ -56,6 +92,13 @@ export class ItemsComponent implements OnInit {
       // Show option 1
       const btna = <View>this.btna.nativeElement;
       btna.animate({
+        translate: { x: 0, y: -80 },
+        opacity: 1,
+        duration: 280,
+        delay: 0,
+      });
+      const labela = <View>this.labela.nativeElement;
+      labela.animate({
         translate: { x: 0, y: -80 },
         opacity: 1,
         duration: 280,
@@ -79,9 +122,203 @@ export class ItemsComponent implements OnInit {
             delay: 0,
           })
         );
+
+      const labelb = <View>this.labelb.nativeElement;
+      labelb
+        .animate({
+          translate: { x: 0, y: -80 },
+          opacity: 1,
+          duration: 280,
+          delay: 0,
+        })
+        .then(() =>
+          labelb.animate({
+            translate: { x: 0, y: -145 },
+            opacity: 1,
+            duration: 280,
+            delay: 0,
+          })
+        );
       // btnb.animate({ translate: { x: 0, y: -65 }, opacity: 1, duration: 280, delay: 140 });
 
+      // Show option 3
+      const btnc = <View>this.btnc.nativeElement;
+      btnc
+        .animate({
+          translate: { x: 0, y: -80 },
+          opacity: 1,
+          duration: 280,
+          delay: 0,
+        })
+        .then(() =>
+          btnc.animate({
+            translate: { x: 0, y: -210 },
+            opacity: 1,
+            duration: 280,
+            delay: 0,
+          })
+        );
+
+      const labelc = <View>this.labelc.nativeElement;
+      labelc
+        .animate({
+          translate: { x: 0, y: -80 },
+          opacity: 1,
+          duration: 280,
+          delay: 0,
+        })
+        .then(() =>
+          labelc.animate({
+            translate: { x: 0, y: -210 },
+            opacity: 1,
+            duration: 280,
+            delay: 0,
+          })
+        );
+      // btnb.animate({ translate: { x: 0, y: -65 }, opacity: 1, duration: 280, delay: 140 });
       this._isFabOpen = true;
+    }
+  }
+
+  displayOptions2() {
+    if (this._isFabOpen === true) {
+      let animations = [
+        {
+          target: <View>this.fab.nativeElement,
+          rotate: 0,
+          duration: 280,
+          delay: 0,
+        },
+        {
+          target: <View>this.btna.nativeElement,
+          translate: { x: 0, y: 0 },
+          opacity: 0,
+          duration: 280,
+          curve: CoreTypes.AnimationCurve.easeOut,
+          delay: 0,
+        },
+        {
+          target: <View>this.labela.nativeElement,
+          translate: { x: 0, y: 0 },
+          opacity: 0,
+          duration: 280,
+          curve: CoreTypes.AnimationCurve.easeOut,
+          delay: 0,
+        },
+        {
+          target: <View>this.btnb.nativeElement,
+          translate: { x: 0, y: 0 },
+          opacity: 0,
+          duration: 280,
+          curve: CoreTypes.AnimationCurve.easeOut,
+          delay: 0,
+        },
+        {
+          target: <View>this.labelb.nativeElement,
+          translate: { x: 0, y: 0 },
+          opacity: 0,
+          duration: 280,
+          curve: CoreTypes.AnimationCurve.easeOut,
+          delay: 0,
+        },
+        {
+          target: <View>this.btnc.nativeElement,
+          translate: { x: 0, y: 0 },
+          opacity: 0,
+          duration: 280,
+          curve: CoreTypes.AnimationCurve.easeOut,
+          delay: 0,
+        },
+        {
+          target: <View>this.labelc.nativeElement,
+          translate: { x: 0, y: 0 },
+          opacity: 0,
+          duration: 280,
+          curve: CoreTypes.AnimationCurve.easeOut,
+          delay: 0,
+        },
+        // { target: <View>this.btnd.nativeElement, translate: { x: 0, y: 0 }, opacity: 0, duration: 280, curve: CoreTypes.AnimationCurve.easeOut, delay: 0 },
+        // { target: <View>this.btne.nativeElement, translate: { x: 0, y: 0 }, opacity: 0, duration: 280, curve: CoreTypes.AnimationCurve.easeOut, delay: 0 }
+      ];
+
+      const a = new Animation(animations);
+      a.play()
+        .then(() => {
+          this._isFabOpen = false;
+        })
+        .catch(function (e) {
+          console.log(e.message);
+        });
+    } else {
+      const appearDelay = 70;
+      let animations = [
+        {
+          target: <View>this.fab.nativeElement,
+          rotate: 45 + 90,
+          duration: 280,
+          delay: 0,
+        },
+        {
+          target: <View>this.btna.nativeElement,
+          translate: { x: 0, y: -80 },
+          opacity: 1,
+          duration: 280,
+          curve: CoreTypes.AnimationCurve.easeOut,
+          delay: appearDelay,
+        },
+        {
+          target: <View>this.labela.nativeElement,
+          translate: { x: 0, y: -80 },
+          opacity: 1,
+          duration: 280,
+          curve: CoreTypes.AnimationCurve.easeOut,
+          delay: appearDelay,
+        },
+        {
+          target: <View>this.btnb.nativeElement,
+          translate: { x: 0, y: -145 },
+          opacity: 1,
+          duration: 280,
+          curve: CoreTypes.AnimationCurve.easeOut,
+          delay: appearDelay,
+        },
+        {
+          target: <View>this.labelb.nativeElement,
+          translate: { x: 0, y: -145 },
+          opacity: 1,
+          duration: 280,
+          curve: CoreTypes.AnimationCurve.easeOut,
+          delay: appearDelay,
+        },
+        {
+          target: <View>this.btnc.nativeElement,
+          translate: { x: 0, y: -210 },
+          opacity: 1,
+          duration: 280,
+          curve: CoreTypes.AnimationCurve.easeOut,
+          delay: appearDelay,
+        },
+        {
+          target: <View>this.labelc.nativeElement,
+          translate: { x: 0, y: -210 },
+          opacity: 1,
+          duration: 280,
+          curve: CoreTypes.AnimationCurve.easeOut,
+          delay: appearDelay,
+        },
+        // { target: <View>this.btnd.nativeElement, translate: { x: 75, y: -80 }, opacity: 1, duration: 280, curve: CoreTypes.AnimationCurve.easeOut, delay: 0 },
+        // { target: <View>this.btne.nativeElement, translate: { x: 90, y: -15 }, opacity: 1, duration: 280, curve: CoreTypes.AnimationCurve.easeOut, delay: 0 }
+      ];
+
+      let a = new Animation(animations);
+
+      a.play()
+        .then(() => {
+          this._isFabOpen = true;
+        })
+        .catch(function (e) {
+          console.log(e.message);
+        });
     }
   }
 }
